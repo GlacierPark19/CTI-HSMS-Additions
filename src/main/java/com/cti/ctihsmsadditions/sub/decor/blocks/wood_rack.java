ded92340e68b844fed580c8469937a70262772c0
GlacierPark19 <55335173+GlacierPark19@users.noreply.github.com>
2021-10-17 16:57:25 -0700

    Model work

--- src/main/java/com/cti/ctihsmsadditions/sub/decor/blocks/wood_rack.java
+++ src/main/java/com/cti/ctihsmsadditions/sub/decor/blocks/wood_rack.java
@@ -1,8 +1,14 @@
 package com.cti.ctihsmsadditions.sub.decor.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraftforge.common.ToolType;
 
 public class wood_rack extends Block {
@@ -16,6 +22,5 @@ public class wood_rack extends Block {
                 .harvestLevel(2)
         );
     }
-
 
 }
